7d13401b003bedf03e1acef8d73afab0d8523c43
esqu1 <brandbest1@gmail.com>
2018-04-12 21:30:35 -0400

    feature extraction

--- classifiers.py
+++ classifiers.py
@@ -13,26 +13,20 @@ def scrape_ids(id):
     r = requests.get(new_url)
     return map(lambda x: int(x['steamid']), r.json()['friendslist']['friends'])
 
-def main():
+def scrape_features():
     with open('user_ids.txt', 'r') as f:
         l = list(set(map(int, f.read().split())))
-        # extract number of friends each
-        # number of achievements for a game
-        # ^ get games per person by GetOwnedGames
 
     user_games = {}
     for i in l:
         print i
         new_url = BASE_URL + '/IPlayerService/GetOwnedGames/v0001/?key=' + key + '&steamid=' + str(i) + '&format=json'
         r = requests.get(new_url)
-        # filter out games that they bought but haven't played?
 
         data = r.json()['response']
         try:
             games = [j for j in data['games'] if j['playtime_forever'] != 0]
-            # user_vector = [0] * 3000
-            # for j in games:
-            #     pass
+            user_vector = [0] * 3000
             user_games[i] = games
         except:
             continue
@@ -42,12 +36,26 @@ def main():
 
     print len(user_games)
 
-main()
+#main()
+
+def main():
+    with open('user_games.json', 'r') as f:
+        games = json.loads(f.read())
+    X, users = [], []
+    for user in games.keys():
+        sparse_vector = [0] * 70000
+        for i in games[user]:
+            if i['appid'] < 700000:
+                sparse_vector[i['appid'] / 10] = i['playtime_forever']
+        X.append(sparse_vector)
+        users.append(user)
+    print X, users
 
 def classify_hours(X, user, k, game_id):
     X = np.array(X)
     dist = np.sum(np.square(X - user), 2)
     knn = np.argpartition(dist, k)
-
     neighbor_hours = X[knn, game_id - 1]
     return np.sum(neighbor_hours)[0] / len(neighbor_hours)
+
+print main()

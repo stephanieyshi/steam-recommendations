df1854f0f0437c6503acf6869cc1a06ef2e70429
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-04-16 12:21:22 -0400

    small data edits

---
+++ data_processing/make_small_data.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import pickle
+import numpy as np
+import scipy.sparse as sparse
+import math
+
+# Parameters
+# Path to git repo on your machine
+directory_path = "/Users/stephanieshi/Documents/Projects/cis520/"
+n = 40  # Number of users
+
+users_map = {}
+with open(directory_path + 'data/train_users.p', 'rb') as f:
+    users_map = pickle.load(f)
+    f.close()
+
+users_map = dict(list(users_map.items())[:n])
+
+all_games = {}
+game_counter = 0
+for user, game_dict in users_map.items():
+    for game in game_dict.keys():
+        if game not in all_games:
+            all_games[game] = game_counter
+            game_counter = game_counter + 1
+
+pickle_out = open(directory_path + 'data/small_user_map.p', 'wb')
+pickle.dump(users_map, pickle_out)
+pickle_out.close()
+
+pickle_out = open(directory_path + 'data/small_games.p', 'wb')
+pickle.dump(all_games, pickle_out)
+pickle_out.close()

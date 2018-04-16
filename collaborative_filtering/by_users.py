349bd116f443bad52b64df3827301860248524ad
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-04-16 10:18:03 -0400

    adding load data code

--- collaborative_filtering/by_users.py
+++ collaborative_filtering/by_users.py
@@ -1,26 +1,50 @@
 #!/usr/bin/env python3
 import pickle
+import numpy
+import scipy.stats
 
 # variables
 FILE_PATH = "./data/"
 
 
+def main():
+    dev_users, train_users, test_users = load_data()
+    learn(dev_users)
+    print('DONE')
+
+
 def load_data():
-    with open(FILE_PATH + 'train_users.p', 'rb') as f:
-        s = pickle.load(f)
     print('LOADING DATA...')
-
-    # TODO: load data
+    with open(FILE_PATH + 'dev_users.p', 'rb') as f:
+        dev_users = pickle.load(f)
+    with open(FILE_PATH + 'train_users.p', 'rb') as f:
+        train_users = pickle.load(f)
+    with open(FILE_PATH + 'test_users.p', 'rb') as f:
+        test_users = pickle.load(f)
     print('DATA LOADED')
+    return dev_users, train_users, test_users
 
 
-def learn():
-    print('hello')
+def learn(users):
+    print('LEARNING...')
+    # initialize matrix
+    n = len(users)
+    R = numpy.zeros(n, n) # n x n matrix of zeros
+
+    for key, value in get_items(users):
+        continue
+
     # TODO: calculate mean rating for each user
 
     # TODO: calculate Pearson correlation coefficient
+    # use scipy.stats.pearsonr(x, y)
 
     # TODO: calculate neighborhood-based predictor function
+    print('MODEL LEARNED')
+
+
+def predict():
+    print('PREDICTING')
 
 
 def evaluate():
@@ -29,5 +53,10 @@ def evaluate():
     # TODO: calculate RMSE
 
 
-load_data()
-print('DONE')
+def get_items(dict_object):
+    for key in dict_object:
+        yield key, dict_object[key]
+
+
+# run
+main()

66204e6f43fae7ab319921141f3c6090ef1da698
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-04-16 12:14:31 -0400

    linter

--- data_processing/4-shrink_mat.py
+++ data_processing/4-shrink_mat.py
@@ -7,5 +7,4 @@ epsilon = .01
 users_mat = sparse.load_npz('./data/users_mat.npz')
 n = users_mat.shape[0]
 games = users_mat.shape[1]
-k = math.ceil(math.log(n/(epsilon^2), 2)
-
+k = math.ceil(math.log(n / (epsilon ^ 2), 2)

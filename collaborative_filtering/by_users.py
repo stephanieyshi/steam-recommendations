d194d5837851f4375cbd669b67c0d7683e025e86
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-04-16 09:37:49 -0400

    in progress

--- collaborative_filtering/by_users.py
+++ collaborative_filtering/by_users.py
@@ -1,25 +1,33 @@
-#!/usr/bin/python
-import json
-import numpy as np
+#!/usr/bin/env python3
+import pickle
 
 # variables
 FILE_PATH = "./data/"
 
+
 def load_data():
+    with open(FILE_PATH + 'train_users.p', 'rb') as f:
+        s = pickle.load(f)
     print('LOADING DATA...')
+
     # TODO: load data
     print('DATA LOADED')
 
-def learn():
 
+def learn():
+    print('hello')
     # TODO: calculate mean rating for each user
 
-    print('done')
+    # TODO: calculate Pearson correlation coefficient
+
+    # TODO: calculate neighborhood-based predictor function
+
 
 def evaluate():
+    print('hello')
     # TODO: calculate MAE
     # TODO: calculate RMSE
 
+
 load_data()
-learn()
-evaluate()
+print('DONE')

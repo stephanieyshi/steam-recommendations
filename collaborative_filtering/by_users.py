99393187327e650578984bd8f3e42a4bb6677697
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-04-16 00:58:15 -0400

    add todos

---
+++ collaborative_filtering/by_users.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import json
+import numpy as np
+
+# variables
+FILE_PATH = "./data/"
+
+def load_data():
+    print('LOADING DATA...')
+    # TODO: load data
+    print('DATA LOADED')
+
+def learn():
+
+    # TODO: calculate mean rating for each user
+
+    print('done')
+
+def evaluate():
+    # TODO: calculate MAE
+    # TODO: calculate RMSE
+
+load_data()
+learn()
+evaluate()

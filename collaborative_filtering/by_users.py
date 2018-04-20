796eb578383116f4a597a5da151f2b8ec84c3673
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-04-20 02:21:38 -0400

    possible kNN for pearsons??

--- collaborative_filtering/by_users.py
+++ collaborative_filtering/by_users.py
@@ -4,6 +4,7 @@ import numpy as np
 import math
 import scipy.stats
 import scipy.sparse as sparse
+from statistics import mean
 
 # variables
 FILE_PATH = "../data/"
@@ -12,15 +13,12 @@ FILE_PATH = "../data/"
 def main():
     data = load_data('user_mat.npz')
     similarity = learn_all(data)
-    print(similarity[0])
-    inx = get_top_k(similarity[0], 2)
-    print('INDICES')
-    print(inx)
-    print('DONE')
+    similar_users = get_top_k(similarity[0], 2, 0)
+    prediction = predict(data, similarity, similar_users, 1, 3)
+    print(prediction)
 
 
 def load_data(file_name):
-    print('LOADING DATA')
     return sparse.load_npz(FILE_PATH + file_name)
 
 
@@ -30,12 +28,13 @@ def learn_all(data):
     sim_pearson = np.zeros((num_users, num_users))
 
     for u1 in range(num_users):
-        sim_pearson[u1] = learn_row(u1, data, num_users)
+        sim_pearson[u1] = learn_row(u1, data)
 
     return sim_pearson
 
 
-def learn_row(row_inx, A, length):
+def learn_row(row_inx, A):
+    length, width = np.shape(A)
     data_1 = A[row_inx, :].A[0]
     arr = []
     for i in range(length):
@@ -48,17 +47,35 @@ def learn_row(row_inx, A, length):
     return arr
 
 
-# returns indices of top k closest users
-def get_top_k(A, k):
-    return np.argpartition(A, -k)
+# returns indices of top k closest users in ascending order, excluding i (b/c same user )
+def get_top_k(A, k, i):
+    temp = np.copy(A)
+    temp[i] = -2 # all other elements are [-1, 1]
+    ind = np.argpartition(temp, -k)[-k:]
+    return ind[np.argsort(temp[ind])[::-1]]
 
 
-def predict(data, similar_users, user_id, game_id):
-    print('PREDICTING')
+# indices of similar users
+def predict(data, similarity, similar_users, user_inx, game_inx):
     other_predictions = []
 
-    # TODO:
+    # find mean of prediction of this user
+    mu = mean_prediction(data, user_inx)
 
+    sum = 0
+    for other_user in similar_users:
+        # compute mean of prediction of this user
+        m = mean_prediction(data, other_user)
+        sum += similarity[user_inx, other_user] * (data[other_user, game_inx] - m)
+
+    # want to predict
+    denom = np.sum(np.absolute(similarity)[similar_users])
+
+    return mu + sum / denom
+
+
+def mean_prediction(data, user_inx):
+    return mean(data[user_inx, :].A[0])
 
 
 # run

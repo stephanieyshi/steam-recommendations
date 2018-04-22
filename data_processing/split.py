9be283c22e9f566d33f01b5c6db347e614bf9387
Barry Plunkett <eplu@sas.upenn.edu>
2018-04-22 01:07:45 -0400

    stashing

---
+++ data_processing/split.py
@@ -0,0 +1,95 @@
+import pickle
+import random
+import scipy
+import re
+import math
+
+# Parameter
+directory_path = "C:/Users/bpiv4/Dropbox/CIS520/cis520/"
+size_test = 20000
+
+# open users map
+users = {}
+total = 0
+
+with open(directory_path + 'data/train_users.csv', 'rb') as f:
+    for line in f:
+        line = line.decode('utf-8')
+        line = line.strip()
+        line = re.split('\t+', line)
+        user = line[0].strip()
+        game = line[1].strip()
+        hours = line[2].strip()
+        total = total + 1
+        if user not in users:
+            game_dict = {}
+            game_dict[game] = float(hours)
+            users[user] = game_dict
+        else:
+            game_dict = users[user]
+            game_dict[game] = float(hours)
+            users[user] = game_dict
+    f.close()
+
+with open(directory_path + 'data/test_users.csv', 'rb') as f:
+    for line in f:
+        line = line.decode('utf-8')
+        line = line.strip()
+        line = re.split('\t+', line)
+        user = line[0].strip()
+        game = line[1].strip()
+        hours = line[2].strip()
+        total = total + 1
+        if user not in users:
+            game_dict = {}
+            game_dict[game] = hours
+            users[user] = game_dict
+        else:
+            game_dict = users[user]
+            game_dict[game] = hours
+            users[user] = game_dict
+    f.close()
+
+train_users = {}
+train_games = {}
+
+test_users = {}
+test_games = {}
+
+test_game_count = 0
+train_game_count = 0
+for user, game_dict in users.items():
+    for game, hours in game_dict.items():
+        decision = random.random()
+        if decision < .1:
+            if game not in test_games:
+                test_games[game] = test_game_count
+                test_game_count = test_game_count + 1
+            if user not in test_users:
+                test_users[user] = {}
+            test_users[user][game] = hours
+        else:
+            if game not in train_games:
+                train_games[game] = train_game_count
+                train_game_count = train_game_count + 1
+            if user not in train_users:
+                train_users[user] = {}
+            train_users[user][game] = hours
+
+print(len(train_users))
+print(len(test_users))
+pickle_out = open(directory_path + "data/train_games.p", 'wb')
+pickle.dump(train_games, pickle_out)
+pickle_out.close()
+
+pickle_out = open(directory_path + "data/test_games.p", 'wb')
+pickle.dump(test_games, pickle_out)
+pickle_out.close()
+
+pickle_out = open(directory_path + "data/train_users.p", 'wb')
+pickle.dump(train_users, pickle_out)
+pickle_out.close()
+
+pickle_out = open(directory_path + "data/test_users.p", 'wb')
+pickle.dump(test_users, pickle_out)
+pickle_out.close()
--- ensemble/1-bias.py
+++ ensemble/1-bias.py
@@ -1,5 +1,5 @@
-import numpy as np 
-import pickle 
+import numpy as np
+import pickle
 import math
 import scipy.sparse as sparse
 import scipy.stats as stats
@@ -7,28 +7,28 @@ import matplotlib.pyplot as plt
 import scipy.optimize as optimize
 
 # Parameters
-directory_path = "C:/Users/bpiv4/Dropbox/CIS520/cis520/"
-epochs = 5 # number of iterations over observations
-eta = .01 # grad descent step size
+directory_path = "./../"
+epochs = 3  # number of iterations over observations
+eta = .0001  # grad descent step size
 
-with open(directory_path + 'data/games.p', 'rb') as f: 
-  games = pickle.load(f)
-  f.close()
+with open(directory_path + 'data/train_games_04.p', 'rb') as f:
+    games = pickle.load(f)
+    f.close()
 
 users_map = {}
-with open(directory_path + 'data/user_map.p', 'rb') as f: 
-  users_map = pickle.load(f)
-  f.close()
+with open(directory_path + 'data/train_user_map_04.p', 'rb') as f:
+    users_map = pickle.load(f)
+    f.close()
 
 users = {}
-with open(directory_path + 'data/users.p', 'rb') as f: 
-  users = pickle.load(f)
-  f.close()
+with open(directory_path + 'data/train_users_04.p', 'rb') as f:
+    users = pickle.load(f)
+    f.close()
 
-users_mat = sparse.load_npz(directory_path + 'data/user_mat.npz')
+users_mat = sparse.load_npz(directory_path + 'data/train_user_mat_04.npz')
 entries = users_mat.data
 
-#Global mean
+# Global mean
 mu = np.mean(entries)
 
 users_mat.data = users_mat.data - mu
@@ -38,30 +38,34 @@ users_mat = users_mat.tocoo()
 num_users = users_mat.shape[0]
 num_games = users_mat.shape[1]
 
+
 def bias_func(users_mat, user_bias, game_bias):
-  total = 0
-  for user, item, rui in zip(users_mat.row, users_mat.col, users_mat.data):
-    # print(user_bias[0, user])
-    # print(game_bias[0, item])
-    # print(type(user_bias[0, user]))
-    # print(type(game_bias[0, item]))
-    curr = (rui - user_bias[0, user] - game_bias[0, item])**2
-    total = total + curr
-  
-  # regularization
-  # user_norm = np.linalg.norm(user_bias, 2)
-  # item_norm = np.linalg.norm(item_bias, 2)
-  # total = total + reg_param * (user_norm + item_norm)
-  return total
+    total = 0
+    for user, item, rui in zip(users_mat.row, users_mat.col, users_mat.data):
+        # print(user_bias[0, user])
+        # print(game_bias[0, item])
+        # print(type(user_bias[0, user]))
+        # print(type(game_bias[0, item]))
+        curr = (rui - user_bias[0, user] - game_bias[0, item])**2
+        total = total + curr
+
+    # regularization
+    # user_norm = np.linalg.norm(user_bias, 2)
+    # item_norm = np.linalg.norm(item_bias, 2)
+    # total = total + reg_param * (user_norm + item_norm)
+    return total
+
 
 print("Beginning prior")
 
 temp_dense = users_mat.todense()
-user_bias = np.divide(np.sum(temp_dense, axis=1), np.count_nonzero(temp_dense, axis=1))
+user_bias = np.divide(np.sum(temp_dense, axis=1),
+                      np.count_nonzero(temp_dense, axis=1))
 user_bias = np.squeeze(np.reshape(user_bias, (num_users, 1))).flatten()
 print(user_bias)
 
-game_bias = np.divide(np.sum(temp_dense, axis=0), np.count_nonzero(temp_dense, axis=0))
+game_bias = np.divide(np.sum(temp_dense, axis=0),
+                      np.count_nonzero(temp_dense, axis=0))
 game_bias = np.squeeze(np.reshape(game_bias, (num_games, 1))).flatten()
 print(game_bias)
 
@@ -92,18 +96,22 @@ print("Intelligent prior done")
 print(game_bias.shape)
 print(user_bias.shape)
 
-print("Objective function value: " + str(bias_func(users_mat, user_bias, game_bias)))
+print("Objective function value: " +
+      str(bias_func(users_mat, user_bias, game_bias)))
 for i in range(epochs):
-  update_bi = np.zeros(game_bias.shape)
-  update_bu = np.zeros(user_bias.shape)
-  for user, item, rui in zip(users_mat.row, users_mat.col, users_mat.data):
-    update_bi[0, item] = update_bi[0, item] + 2 * eta * (rui - user_bias[0, user] - game_bias[0, item])
-    update_bu[0, user] = update_bu[0, user] + 2 * eta * (rui - user_bias[0, user] - game_bias[0, item])
-  user_bias = update_bu + user_bias
-  game_bias = update_bi + game_bias
-  print("Objective function value: " + str(bias_func(users_mat, user_bias, game_bias)))
+    update_bi = np.zeros(game_bias.shape)
+    update_bu = np.zeros(user_bias.shape)
+    for user, item, rui in zip(users_mat.row, users_mat.col, users_mat.data):
+        update_bi[0, item] = update_bi[0, item] + 2 * eta * \
+            (rui - user_bias[0, user] - game_bias[0, item])
+        update_bu[0, user] = update_bu[0, user] + 2 * eta * \
+            (rui - user_bias[0, user] - game_bias[0, item])
+    user_bias = update_bu + user_bias
+    game_bias = update_bi + game_bias
+    print("Objective function value: " +
+          str(bias_func(users_mat, user_bias, game_bias)))
 
-pickle_out = open(directory_path + "data/bias.p", 'wb')
+pickle_out = open(directory_path + "data/train_bias_04.p", 'wb')
 pickle.dump(mu, pickle_out)
 pickle.dump(user_bias, pickle_out)
 pickle.dump(game_bias, pickle_out)
